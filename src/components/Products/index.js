124da4da93a08ab89983a47c06faafd638bfae27
Ricardo Dias <ricardodias.dev@gmail.com>
2023-03-22 10:24:58 -0300

    Listando produtos e acessando página Cart

---
+++ src/components/Products/index.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React from "react";
+
+export default function Product({ data }) {
+  const { name, price } = data;
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.title}>{name}</Text>
+        <Text>R$ {price.toFixed(2)} </Text>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text>+</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: "#DFDFDF",
+    borderRadius: 2,
+    marginBottom: 14,
+    padding: 8,
+    paddingBottom: 14,
+    paddingTop: 14,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontWeight: "bold",
+  },
+  button: {
+    paddingStart: 12,
+    paddingEnd: 12,
+    backgroundColor: "#168fff",
+    paddingTop: 6,
+    paddingBottom: 6,
+    borderRadius: 2,
+  },
+});
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,7 +9,12 @@ import {
 import React, { useState } from "react";
 import { Feather } from "@expo/vector-icons";
 
+import { useNavigation } from "@react-navigation/native";
+
+import Product from "../../components/Products";
+
 export default function Home() {
+  const navigation = useNavigation();
   const [products, setProducts] = useState([
     {
       id: "1",
@@ -41,13 +46,22 @@ export default function Home() {
     <SafeAreaView style={styles.Container}>
       <View style={styles.cartContent}>
         <Text style={styles.title}>Lista de produtos</Text>
-        <TouchableOpacity style={styles.cartButton}>
+        <TouchableOpacity
+          style={styles.cartButton}
+          onPress={() => navigation.navigate("Cart")}
+        >
           <View style={styles.dot}>
             <Text style={styles.dotText}>3</Text>
           </View>
-          <Feather name="shopping-cart" size={30} color="#000"/>
+          <Feather name="shopping-cart" size={30} color="#000" />
         </TouchableOpacity>
       </View>
+      <FlatList
+        style={styles.list}
+        data={products}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => <Product data={item} />}
+      />
     </SafeAreaView>
   );
 }
@@ -58,33 +72,30 @@ const styles = StyleSheet.create({
     paddingEnd: 14,
     paddingStart: 14,
   },
-  cartContent:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+  cartContent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginTop: 24,
     marginBottom: 24,
   },
-  title:{
-    fontSize: 24, 
-    fontWeight: 'bold'
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
   },
-  cartButton:{},
-  dot:{ 
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'red',
+  dot: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "red",
     width: 20,
     height: 20,
     borderRadius: 12,
-    position: 'absolute',
+    position: "absolute",
     zIndex: 99,
     bottom: -2,
-    left: -4
-
-},
-  dotText:{
+    left: -4,
+  },
+  dotText: {
     fontSize: 12,
   },
-
 });
